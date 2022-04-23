26bfeeb938efb0e6349e1dce8ef4a91fded3ac3e
Lê Thiện <ltnthien2301@gmail.com>
2022-04-23 20:28:34 +0700

    add CRUD for all domain

--- SV19T1021254.BussinessLayer/CommonDataService.cs
+++ SV19T1021254.BussinessLayer/CommonDataService.cs
@@ -56,12 +56,61 @@ namespace SV19T1021254.BussinessLayer
             return categoryDB.List(page, pageSize, searchValue).ToList();
         }
         /// <summary>
+        /// Lấy thông tin 1 loại hàng
+        /// </summary>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public static Category GetCategory(int categoryID)
+        {
+            return categoryDB.Get(categoryID);
+        }
+        /// <summary>
+        /// Thêm một mặt hàng
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static int AddCategory(Category data)
+        {
+            return categoryDB.Add(data);
+        }
+        /// <summary>
+        /// Cập nhật thông tin một loại hàng
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool UpdateCategory(Category data)
+        {
+            return categoryDB.Update(data);
+        }
+        /// <summary>
+        /// Kiểm tra xem loại hàng có mặt hàng nào không
+        /// </summary>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public static bool InUsedCategory(int categoryID)
+        {
+            return categoryDB.InUsed(categoryID);
+        }
+        /// <summary>
+        /// Xoá một loại hàng khi nó không tồn tại một mặt hàng nào
+        /// </summary>
+        /// <param name="categoryID"></param>
+        /// <returns></returns>
+        public static bool DeleteCategory(int categoryID)
+        {
+            if (categoryDB.InUsed(categoryID))
+                return false;
+            return categoryDB.Delete(categoryID);
+        }
+
+        /// <summary>
         /// Lấy danh sách các quốc gia
         /// </summary>
         /// <returns></returns>
         public static List<Country> ListOfCountries(){
             return countryDB.List().ToList();
         }
+
         /// <summary>
         /// Tìm kiếm và lấy danh sách khách hàng dưới dạng phân trang
         /// </summary>
@@ -74,6 +123,15 @@ namespace SV19T1021254.BussinessLayer
         {
             rowCount = customerDB.Count(searchValue);
             return customerDB.List(page, pageSize, searchValue).ToList();
+        }
+        /// <summary>
+        /// Lấy thông tin 1 khách hàng từ CSDL
+        /// </summary>
+        /// <param name="customerID"></param>
+        /// <returns></returns>
+        public static Customer GetCustomer(int customerID)
+        {
+            return customerDB.Get(customerID);
         }
         /// <summary>
         /// Thêm một khách hàng
@@ -94,26 +152,6 @@ namespace SV19T1021254.BussinessLayer
             return customerDB.Update(data);
         }
         /// <summary>
-        /// Xoá khách hàng, nếu khách đã tồn tại đơn hàng thì không cho phép xoá.
-        /// </summary>
-        /// <param name="customerID"></param>
-        /// <returns></returns>
-        public static bool DelteCustomer(int customerID)
-        {
-            if (customerDB.InUsed(customerID))
-                return false;
-            return customerDB.Delete(customerID);
-        }
-        /// <summary>
-        /// Lấy thông tin 1 khách hàng từ CSDL
-        /// </summary>
-        /// <param name="customerID"></param>
-        /// <returns></returns>
-        public static Customer GetCustomer(int customerID)
-        {
-            return customerDB.Get(customerID);
-        }
-        /// <summary>
         /// Kiểm tra xem khách hàng có đơn hàng không
         /// </summary>
         /// <param name="customerID"></param>
@@ -123,6 +161,18 @@ namespace SV19T1021254.BussinessLayer
             return customerDB.InUsed(customerID);
         }
         /// <summary>
+        /// Xoá khách hàng, nếu như đã tồn tại đơn hàng thì không cho phép xoá.
+        /// </summary>
+        /// <param name="customerID"></param>
+        /// <returns></returns>
+        public static bool DelteCustomer(int customerID)
+        {
+            if (customerDB.InUsed(customerID))
+                return false;
+            return customerDB.Delete(customerID);
+        }
+
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="page"></param>
@@ -131,10 +181,59 @@ namespace SV19T1021254.BussinessLayer
         /// <param name="rowCount"></param>
         /// <returns></returns>
         public static List<Supplier> ListOfSuppliers(int page, int pageSize, string searchValue, out int rowCount)
+
         {
             rowCount = supplierDB.Count(searchValue);
             return supplierDB.List(page, pageSize, searchValue).ToList();
         }
+        /// <summary>
+        /// Lấy thông tin 1 nhà cung cấp từ CSDL
+        /// </summary>
+        /// <param name="supplierID"></param>
+        /// <returns></returns>
+        public static Supplier GetSupplier(int supplierID)
+        {
+            return supplierDB.Get(supplierID);
+        }
+        /// <summary>
+        /// Thêm một nhà cung cấp
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static int AddSupplier(Supplier data)
+        {
+            return supplierDB.Add(data);
+        }
+        /// <summary>
+        /// Cập nhật thông tin nhà cung cấp
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool UpdateSupplier(Supplier data)
+        {
+            return supplierDB.Update(data);
+        }
+        /// <summary>
+        /// Kiểm tra xem nhà cung cấp có mặt hàng không
+        /// </summary>
+        /// <param name="supplierID"></param>
+        /// <returns></returns>
+        public static bool InUsedSupplier(int supplierID)
+        {
+            return supplierDB.InUsed(supplierID);
+        }
+        /// <summary>
+        /// Xoá nhà cung cấp, nếu như đã tồn tại mặt hàng thì không cho phép xoá.
+        /// </summary>
+        /// <param name="supplierID"></param>
+        /// <returns></returns>
+        public static bool DelteSupplier(int supplierID)
+        {
+            if (supplierDB.InUsed(supplierID))
+                return false;
+            return supplierDB.Delete(supplierID);
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -149,6 +248,54 @@ namespace SV19T1021254.BussinessLayer
             return shipperDB.List(page, pageSize, searchValue).ToList();
         }
         /// <summary>
+        /// Lấy thông tin 1 người giao hàng từ CSDL
+        /// </summary>
+        /// <param name="shipperID"></param>
+        /// <returns></returns>
+        public static Shipper GetShipper(int shipperID)
+        {
+            return shipperDB.Get(shipperID);
+        }
+        /// <summary>
+        /// Thêm một người giao hàng
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static int AddShipper(Shipper data)
+        {
+            return shipperDB.Add(data);
+        }
+        /// <summary>
+        /// Cập nhật thông tin người giao hàng
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool UpdateShipper(Shipper data)
+        {
+            return shipperDB.Update(data);
+        }
+        /// <summary>
+        /// Kiểm tra xem người giao hàng có đơn hàng không
+        /// </summary>
+        /// <param name="shipperID"></param>
+        /// <returns></returns>
+        public static bool InUsedShipper(int shipperID)
+        {
+            return shipperDB.InUsed(shipperID);
+        }
+        /// <summary>
+        /// Xoá người giao hàng, nếu như đã tồn tại đơn hàng thì không cho phép xoá.
+        /// </summary>
+        /// <param name="shipperID"></param>
+        /// <returns></returns>
+        public static bool DelteShipper(int shipperID)
+        {
+            if (shipperDB.InUsed(shipperID))
+                return false;
+            return shipperDB.Delete(shipperID);
+        }
+
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="page"></param>
@@ -160,6 +307,53 @@ namespace SV19T1021254.BussinessLayer
         {
             rowCount = employeeDB.Count(searchValue);
             return employeeDB.List(page, pageSize, searchValue).ToList();
+        }
+        /// <summary>
+        /// Lấy thông tin 1 nhân viên từ CSDL
+        /// </summary>
+        /// <param name="employeeID"></param>
+        /// <returns></returns>
+        public static Employee GetEmployee(int employeeID)
+        {
+            return employeeDB.Get(employeeID);
+        }
+        /// <summary>
+        /// Thêm một nhân viên
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static int AddEmployee(Employee data)
+        {
+            return employeeDB.Add(data);
+        }
+        /// <summary>
+        /// Cập nhật thông tin nhân viên
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static bool UpdateEmployee(Employee data)
+        {
+            return employeeDB.Update(data);
+        }
+        /// <summary>
+        /// Kiểm tra xem nhân viên có đơn hàng không
+        /// </summary>
+        /// <param name="employeeID"></param>
+        /// <returns></returns>
+        public static bool InUsedEmployee(int employeeID)
+        {
+            return employeeDB.InUsed(employeeID);
+        }
+        /// <summary>
+        /// Xoá nhân viên, nếu như đã tồn tại đơn hàng thì không cho phép xoá.
+        /// </summary>
+        /// <param name="employeeID"></param>
+        /// <returns></returns>
+        public static bool DelteEmployee(int employeeID)
+        {
+            if (employeeDB.InUsed(employeeID))
+                return false;
+            return employeeDB.Delete(employeeID);
         }
     }
 }
